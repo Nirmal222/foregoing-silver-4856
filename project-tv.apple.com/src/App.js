e12d7e06d71176cb459a8815b84144a1553f8896
Nirmal Kumar <nirmalkumargurajada@gmail.com>
2022-08-24 15:36:45 +0530

    Day1

---
+++ project-tv.apple.com/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import AllRoutes from './Routes/AllRoutes';
+function App() {
+  return (
+    <div>
+      <AllRoutes/>
+    </div>
+  );
+}
+
+export default App;
---
+++ project-tv.apple.com/src/components/HomePageComponents/Banner.jsx
@@ -0,0 +1,37 @@
+import { Box, Button, Text } from "@chakra-ui/react";
+import React from "react";
+import {TriangleDownIcon } from "@chakra-ui/icons";
+
+const Banner = () => {
+  return (
+    <div>
+      <Box
+        backgroundImage="url('https://is2-ssl.mzstatic.com/image/thumb/Features112/v4/a1/fb/e7/a1fbe76c-a6b8-248b-e19a-63bc94588caf/0ab1a582-f500-4585-a68e-5c5c7c6e1b7c.png/1998x1124sr.jpg')"
+        backgroundPosition="center"
+        backgroundRepeat="no-repeat"
+        backgroundSize={'cover'}
+        h={'100vh'}
+        minW={['320px','480px','740px']}
+        position={'relative'}
+      >
+        <Box 
+        color={'white'} 
+        position={'absolute'}
+        bottom={'8%'}
+        left={'50%'}
+        transform={'translate(-50%)'}
+        textAlign={'center'}
+        >
+            <Text as={'h1'} fontSize={['60px','80px','80px','100px']} w={'100vw'} fontWeight={"700"}>All Apple Originals.</Text>
+            <Text as={'h1'} fontSize={['10px','12px','15px','20px']} w={'100vw'} >Exclusively on Apple TV+. Watch here and on the Apple TV app across your devices.</Text>
+            <Button color={'#141a10'} bg='#ffffff' mt={'20px'}>Start Free Trial</Button>
+            <Text mt={'20px'} as={'p'}>7&nbsp;days free, then $4.99/month.</Text>
+            <Text><span >Or 3 months free when you buy an eligible Apple device.&nbsp;Some titles coming later to Apple&nbsp;TV+.</span></Text>
+        </Box>
+      <TriangleDownIcon position={'absolute'} bottom={'2%'} left={'50%'} color={'white'}/>
+      </Box>
+    </div>
+  );
+};
+
+export default Banner;
